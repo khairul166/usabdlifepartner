ce3543629840e2e23e4fcee93d00db9af57b6ada
khairul166 <khirulislam166@gmail.com>
2025-09-03 01:46:18 +0600

    aaaaaaa

--- about.php
+++ about.php
@@ -19,20 +19,13 @@ $phone = get_theme_mod('contact_phone', '+1 234 567 890');
 $email = get_theme_mod('contact_email', 'info@yourdomain.com');
 // Count all approved users
 $approved_users = count(get_users([
-<<<<<<< HEAD
 	'meta_key' => 'approval_status',
 	'meta_value' => 'approved',
 	'number' => -1,
-=======
-    'meta_key' => 'approval_status',
-    'meta_value' => 'approved',
-    'number' => -1,
->>>>>>> 523a812 (Initial commit after system restore)
 ]));
 
 // Count approved male users
 $approved_males = count(get_users([
-<<<<<<< HEAD
 	'meta_query' => [
 		[
 			'key' => 'approval_status',
@@ -46,26 +39,10 @@ $approved_males = count(get_users([
 		]
 	],
 	'number' => -1,
-=======
-    'meta_query' => [
-        [
-            'key' => 'approval_status',
-            'value' => 'approved',
-            'compare' => '='
-        ],
-        [
-            'key' => 'user_gender',
-            'value' => 'Male',
-            'compare' => '='
-        ]
-    ],
-    'number' => -1,
->>>>>>> 523a812 (Initial commit after system restore)
 ]));
 
 // Count approved female users
 $approved_females = count(get_users([
-<<<<<<< HEAD
 	'meta_query' => [
 		[
 			'key' => 'approval_status',
@@ -79,26 +56,10 @@ $approved_females = count(get_users([
 		]
 	],
 	'number' => -1,
-=======
-    'meta_query' => [
-        [
-            'key' => 'approval_status',
-            'value' => 'approved',
-            'compare' => '='
-        ],
-        [
-            'key' => 'user_gender',
-            'value' => 'Female',
-            'compare' => '='
-        ]
-    ],
-    'number' => -1,
->>>>>>> 523a812 (Initial commit after system restore)
 ]));
 
 // Count success stories (published)
 $success_stories = wp_count_posts('success_story')->publish;
-<<<<<<< HEAD
 
 $opt_name = 'theme_admin_settings';
 $theme_options = get_option($opt_name);
@@ -143,155 +104,6 @@ $faq_right_image = $theme_options['faq_right_image'] ?? [];
 ?>
 
 <section id="center" class="center_about pt-4 theme-bg">
-=======
-?>
-
-<section id="center" class="center_about pt-4 theme-bg">
-   <div class="container-xl">
-    <div class="row center_price1 text-center">
-	  <div class="col-md-12">
-	   <b class="fs-5 d-block mb-3 text-warning">#1 WEDDING WEBSITE</b>
-	   <h1 class="text-white font_50">About Us</h1>
-<p class="mt-3 text-white mb-0 fs-5">Most Trusted and best Matrimony Service in the World.</p>
-	  </div>
-	</div>
-   </div>
- </section>
- 
- <section id="about_pg" class="pb-5">
-   <div class="container-xl">
-    <div class="row row-cols-1 row-cols-md-3 about_pg1  mx-auto">
-	  <div class="col">
-	    <div class="about_pg1_left shadow bg-white p-4 rounded-3 text-center hvr-grow mx-2">
-		   <span class="theme-text-color fs-1 mb-3 d-block"><i class="bi bi-person-check"></i></span>
-		   <b class="d-block fs-5">Genuine profiles</b>
-		   <p class="mb-0 mt-3">The most trusted wedding matrimony brand</p>
-		</div>  
-	  </div>
-	  <div class="col">
-	    <div class="about_pg1_left shadow bg-white p-4 rounded-3 text-center hvr-grow  mx-2">
-		   <span class="theme-text-color fs-1 mb-3 d-block"><i class="bi bi-shield-lock"></i></span>
-		   <b class="d-block fs-5">Most trusted</b>
-		   <p class="mb-0 mt-3">The most trusted wedding matrimony brand</p>
-		</div>  
-	  </div>
-	  <div class="col">
-	    <div class="about_pg1_left shadow bg-white p-4 rounded-3 text-center hvr-grow  mx-2">
-		   <span class="theme-text-color fs-1 mb-3 d-block"><i class="bi bi-arrow-through-heart"></i></span>
-		   <b class="d-block fs-5"><?php echo $success_stories; ?>+ weddings</b>
-		   <p class="mb-0 mt-3">The most trusted wedding matrimony brand</p>
-		</div>  
-	  </div>
-	</div>
-	<div class="row row-cols-1 row-cols-md-2 about_pg2 mt-5">
-	  <div class="col">
-	     <div class="about_pg2_left position-relative">
-		    <div class="about_pg2_left1">
-			  <img src="<?php echo get_template_directory_uri(); ?>/image/32.jpg" alt="abc" class="w-75 rounded-3">
-			</div>
-			<div class="about_pg2_left2 position-absolute bottom-0 text-end">
-			   <img src="<?php echo get_template_directory_uri(); ?>/image/33.jpg" alt="abc" class="w-75">
-			</div>
-		 </div>
-	  </div>
-
-	  <div class="col">
-	     <div class="about_pg2_right">
-		   <h1 class="text-uppercase font_50">Welcome to</h1>
-		   <h2 class="theme-text-color text-uppercase">Wedding matrimony</h2>
-		   <p class="mt-4 mb-4">Best wedding matrimony It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-		   <span><a class="theme-text-color" href="<?php echo esc_url(home_url('/search-user')); ?>">Click here</a> to Start you matrimony service now.</span>
-		   <p class="border-top mt-4 pt-4 mb-4">Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.Vestibulum lacinia arcu eget nulla.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitursodales ligula in libero. Sed dignissim lacinia nunc.</p>
-		    <ul class="mb-0 d-flex justify-content-between flex-wrap">
-			 <li class="d-flex">
-			  <span class="d-inline-block text-center bg_blue text-white rounded-circle cont_icon me-3"><i class="bi bi-telephone"></i></span>
-			   <span class="flex-column">
-			    <span class="text-muted d-block"> Enquiry</span>
-				<b class="d-block fs-5"><?php echo esc_html($phone); ?></b>
-			   </span>
-			 </li>
-			 <li class="d-flex">
-			  <span class="d-inline-block text-center bg_blue text-white rounded-circle cont_icon me-3"><i class="bi bi-envelope"></i></span>
-			   <span class="flex-column">
-			    <span class="text-muted d-block"> Get Support</span>
-				<b class="d-block fs-5"> <?php echo esc_html($email); ?></b>
-			   </span>
-			 </li>
-			</ul>
-		 </div>
-	  </div>
-	</div>
-
-	<?php
-
-?>
-		<div class="row row-cols-1 row-cols-md-4 row-cols-sm-2 about_pg3 mt-5 border-top border-bottom">
-			<div class="col">
-				<div class="about_pg3_left border-end py-4 px-3">
-					<ul class="mb-0 d-flex">
-						<li class="d-flex">
-							<span
-								class="d-inline-block text-center theme-bg text-white rounded-circle cont_icon me-3 fs-5"><i
-									class="bi bi-heart"></i></span>
-							<span class="flex-column lh-1">
-								<b class="fs-1 d-block count" data-target="<?php echo esc_attr($success_stories); ?>">0</b>
-								<span class="d-block text-uppercase text-muted mt-2 font_13">COUPLES PARED</span>
-							</span>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col">
-				<div class="about_pg3_left border-end py-4 px-3">
-					<ul class="mb-0 d-flex">
-						<li class="d-flex">
-							<span
-								class="d-inline-block text-center theme-bg text-white rounded-circle cont_icon me-3 fs-5"><i
-									class="bi bi-people"></i></span>
-							<span class="flex-column lh-1">
-								<b class="fs-1 d-block count" data-target="<?php echo esc_attr($approved_users); ?>">0</b>
-								<span class="d-block text-uppercase text-muted mt-2 font_13">REGISTERED USERS</span>
-							</span>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col">
-				<div class="about_pg3_left border-end py-4 px-3">
-					<ul class="mb-0 d-flex">
-						<li class="d-flex">
-							<span
-								class="d-inline-block text-center theme-bg text-white rounded-circle cont_icon me-3 fs-5"><i
-									class="bi bi-gender-male"></i></span>
-							<span class="flex-column lh-1">
-								<b class="fs-1 d-block count" data-target="<?php echo esc_attr($approved_males); ?>">0</b>
-								<span class="d-block text-uppercase text-muted mt-2 font_13">Mens</span>
-							</span>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col">
-				<div class="about_pg3_left py-4 px-3">
-					<ul class="mb-0 d-flex">
-						<li class="d-flex">
-							<span
-								class="d-inline-block text-center theme-bg text-white rounded-circle cont_icon me-3 fs-5"><i
-									class="bi bi-gender-female"></i></span>
-							<span class="flex-column lh-1">
-								<b class="fs-1 d-block count" data-target="<?php echo esc_attr($approved_females); ?>">0</b>
-								<span class="d-block text-uppercase text-muted mt-2 font_13">WOMENS</span>
-							</span>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-   </div>
- </section>
- 
- <section id="testim" class="pb-5 pt-5 bg_light">
->>>>>>> 523a812 (Initial commit after system restore)
 	<div class="container-xl">
 		<div class="row center_price1 text-center">
 			<div class="col-md-12">
@@ -354,80 +166,8 @@ $faq_right_image = $theme_options['faq_right_image'] ?? [];
 						</li>
 					</ul>
 				</div>
-<<<<<<< HEAD
 			</div>
 		</div>
-=======
-			  </div>
-	  </div>
-	</div>
-  </section>
- 
- 
- <section id="faq" class="pb-5 pt-5 bg_light">
-   <div class="container-xl">
-     <div class="testim_1 row text-center mb-4">
-	  <div class="col-md-12">
-	   <h2 class="text-uppercase">KNOWLEDGE  <span class="theme-text-color">Faq</span></h2>
-	   <span class="text-uppercase">FUSCE IMPERDIET ULLAMCORPER FRINGILLA</span>
-	  </div>
-	 </div>
-	 <div class="testim_2 row row-cols-1 row-cols-md-2">
-	  <div class="col">
-	   <div class="testim_2_left">
-	     <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-   <h2 class="accordion-header" id="headingOne">
-     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-       <i class="bi bi-check-circle me-2 align-middle"></i> It is a long established fact
-     </button>
-   </h2>
-   <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style="">
-     <div class="accordion-body">
-       <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-     </div>
-   </div>
-  </div>
-   
-  <div class="accordion-item">
-   <h2 class="accordion-header" id="headingTwo">
-     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-       <i class="bi bi-check-circle me-2 align-middle"></i> Where can I get some?
-     </button>
-   </h2>
-   <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-     <div class="accordion-body">
-       <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-     </div>
-   </div>
- </div>
-  
-  <div class="accordion-item">
-   <h2 class="accordion-header" id="headingThree">
-     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-       <i class="bi bi-check-circle me-2 align-middle"></i> Where does it come from?
-     </button>
-   </h2>
-   <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-     <div class="accordion-body">
-        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-     </div>
-   </div>
- </div>
- 
- <div class="accordion-item">
-   <h2 class="accordion-header" id="headingFour">
-     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-       <i class="bi bi-check-circle me-2 align-middle"></i> Why do we use it?
-     </button>
-   </h2>
-   <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-     <div class="accordion-body">
-       <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-     </div>
-   </div>
- </div>
->>>>>>> 523a812 (Initial commit after system restore)
 
 		<?php
 
--- team.php
+++ team.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-=======
->>>>>>> 523a812 (Initial commit after system restore)
 /**
  * The main template file
  *
@@ -16,7 +12,6 @@
  * Template Name: Teams
  */
 
-<<<<<<< HEAD
 get_header();
 
 
@@ -114,60 +109,11 @@ get_header();
                     <h6 class="font_14 mb-0 mt-2"><span class="fw-bold">Experience:</span> <?php echo $experience; ?></h6>
                     <p class="mb-0 mt-3"><?php echo $about; ?></p>
                   </div>
-=======
-get_header(); ?>
-
-<section id="center" class="team_pg  pt-5 pb-5">
-   <div class="container-xl">
-	    <h1 class="theme-text-color text-center mb-4">OUR TEAM</h1>
-		<div class="row team_1 mx-3">
-		<?php
-		$team_members = cs_get_option('team_members');
-
-if (!empty($team_members)) :
-  foreach ($team_members as $member) :
-    $name         = esc_html($member['name']);
-    $designation  = esc_html($member['designation']);
-    $qualification = esc_html($member['qualification']);
-    $experience   = esc_html($member['experience']);
-    $about        = esc_html($member['about']);
-    $image        = esc_url($member['image']);
-    $socials = [
-      'facebook'  => $member['facebook'] ?? '',
-      'twitter'   => $member['twitter'] ?? '',
-      'linkedin'  => $member['linkedin'] ?? '',
-      'instagram' => $member['instagram'] ?? '',
-      'youtube'   => $member['youtube'] ?? '',
-    ];
-    ?>
-
-    <div class="col-md-6 mt-4">
-      <div class="team_1i">
-        <div class="team_1i1 row">
-          <div class="col-md-6 col-sm-6 pe-0">
-            <div class="team_1i1l position-relative">
-              <div class="team_1i1li">
-                <a href="#"><img src="<?php echo $image; ?>" class="img-fluid" alt="<?php echo $name; ?>"></a>
-              </div>
-              <div class="team_1i1li1 position-absolute w-100 h-100 bg_back top-0 text-center">
-                <div class="top_1r">
-                  <ul class="mb-0">
-                    <?php foreach ($socials as $platform => $url) :
-                      if (!empty($url)) : ?>
-                        <li class="d-inline-block">
-                          <a class="bg-white d-block rounded-circle text-center theme-text-color" href="<?php echo esc_url($url); ?>" target="_blank">
-                            <i class="bi bi-<?php echo esc_attr($platform); ?>"></i>
-                          </a>
-                        </li>
-                    <?php endif; endforeach; ?>
-                  </ul>
->>>>>>> 523a812 (Initial commit after system restore)
                 </div>
               </div>
             </div>
           </div>
 
-<<<<<<< HEAD
       <?php
         endforeach;
       endif;
@@ -176,27 +122,3 @@ if (!empty($team_members)) :
 </section>
 
 <?php get_footer(); ?>
-=======
-          <div class="col-md-6 col-sm-6 ps-0">
-            <div class="team_1i1r bg_light px-4">
-              <h5><?php echo $name; ?></h5>
-              <h6 class="font_14"><?php echo $designation; ?></h6>
-              <hr class="line">
-              <h6 class="font_14"><span class="fw-bold">Qualification:</span> <?php echo $qualification; ?></h6>
-              <h6 class="font_14 mb-0 mt-2"><span class="fw-bold">Experience:</span> <?php echo $experience; ?></h6>
-              <p class="mb-0 mt-3"><?php echo $about; ?></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  <?php
-  endforeach;
-endif;
-?>
-   </div>
- </section>
-
- <?php get_footer(); ?>
->>>>>>> 523a812 (Initial commit after system restore)
